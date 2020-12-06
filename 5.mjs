d9aafb15d8a29189e0c60ca1755a2f6d2b01e003
Anton Vasin <antonvasin@me.com>
2020-12-06 17:48:30 +0300

    5.2

--- 5.mjs
+++ 5.mjs
@@ -3,21 +3,39 @@ import fs from 'fs';
 let input = await fs.promises.readFile(process.argv[2], 'utf8');
 input = input.split('\n').slice(0, -1);
 
-let passes = [];
+let passes = {};
 let highest = 0;
 
-input.forEach((pass, i) => {
+// part 1
+for (let pass of input) {
   let column = parseInt(pass.substring(7).replace(/L/g, 0).replace(/R/g, 1), 2);
   let row = parseInt(pass.substring(0, 7).replace(/F/g, 0).replace(/B/g, 1), 2);
   let id = row * 8 + column;
 
-  passes[i] = { row, column, id };
+  passes[id] = { row, column, id };
 
   if (id > highest) {
     highest = id;
   }
+}
 
-  // console.log(passes[i]);
-});
+console.log('Highest is:', highest);
 
-console.log('highest is: ', highest);
+// part 2
+let gap = false;
+let init = true;
+for (let i = 0; i < 1024; i++) {
+  let found = passes.hasOwnProperty(i);
+  if (init && found) {
+    init = false;
+    gap = false;
+    continue;
+  }
+
+  if (gap && found) {
+    console.log('Missing is:', i - 1);
+    break;
+  }
+
+  gap = !found;
+}
