de70df12c0649f9f09ecdd92ca8d26b58e133212
Anton Vasin <antonvasin@me.com>
2020-12-06 19:08:28 +0300

    6.2

--- 6.mjs
+++ 6.mjs
@@ -3,26 +3,38 @@ import fs from 'fs';
 let input = await fs.promises.readFile(process.argv[2], 'utf8');
 input = input.split('\n');
 
-let answers = {};
+let groups = [{}];
+let people = [0];
+
 let g = 0;
-
-let count = 0;
-
 for (let line of input) {
-  if (!answers[g]) {
-    answers[g] = {};
-  }
-
   if (line === '') {
+    groups.push({});
+    people.push(0);
+
     g++;
   } else {
     for (let c of line) {
-      if (!answers[g][c]) {
-        count++;
-        answers[g][c] = true;
+      if (!groups[g][c]) {
+        groups[g][c] = 0;
       }
+      groups[g][c]++;
     }
+    people[g]++;
   }
 }
 
-console.log(count);
+let sum1 = 0;
+let sum2 = 0;
+
+for (const [i, gr] of groups.entries()) {
+  for (let count of Object.values(gr)) {
+    if (count === people[i]) {
+      sum2++;
+    }
+  }
+
+  sum1 += Object.values(gr).length;
+}
+
+console.log({ sum1, sum2 });
