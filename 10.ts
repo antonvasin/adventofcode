d686d11f267e55d4ca9f8ae58f384d46a092caad
Anton Vasin <antonvasin@me.com>
2020-12-10 20:08:29 +0300

    10.2

--- 10.ts
+++ 10.ts
@@ -2,16 +2,67 @@ import { promises as fs } from 'fs';
 
 let input = (await fs.readFile(process.argv[2], 'utf8')).split('\n');
 
-let differences: number[] = [];
-let adapters = input.map(Number).sort((a, b) => a - b);
-let myRating = Math.max(...adapters) + 3;
+let voltages = input.map(Number).sort((a, b) => a - b);
+// reverse idx
+let seen: Record<number, number> = {};
 
-for (let [i, adapter] of adapters.entries()) {
-  let next = adapters[i + 1];
-  let diff = next - adapter;
-  differences.push(diff);
+let diffs = {
+  ones: 1,
+  threes: 1,
+};
+
+for (let [i, voltage] of voltages.entries()) {
+  seen[voltage] = i;
+  let next = voltages[i + 1];
+  let diff = next - voltage;
+
+  switch (diff) {
+    case 1:
+      diffs.ones++;
+      break;
+    case 3:
+      diffs.threes++;
+      break;
+    default:
+  }
 }
 
-console.log(
-  (differences.filter((d) => d === 1).length + 1) * (differences.filter((d) => d === 3).length + 1),
-);
+let ways: Record<number, number> = {};
+ways[voltages.length - 1] = 1;
+
+// N is our voltage
+//
+// traverse from back to front, filling memoize obj
+//
+// from the bottom of the list
+// check if +1 +2 or +3 exists
+//   if so then add previously known value to the sum
+// in the end memoize value for current element
+for (let i = voltages.length - 2; i >= 0; i--) {
+  let sum = 0;
+
+  for (let diff = 1; diff <= 3; diff++) {
+    let pos = seen[voltages[i] + diff];
+
+    if (pos) {
+      sum += ways[pos];
+    }
+  }
+
+  ways[i] = sum;
+}
+
+let countTotal = 0;
+
+// known sums at the top (1, 2, 3) are the ways to connect from 0 to N
+for (let v = 1; v <= 3; v++) {
+  let pos = seen[v];
+  if (pos >= 0) {
+    countTotal += ways[pos];
+  }
+}
+
+console.log({
+  result: diffs.ones * diffs.threes,
+  countTotal,
+});
