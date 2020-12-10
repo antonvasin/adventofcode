f3931e1c8e66d1f20fcaade313bc80dc5dd64ee3
Anton Vasin <antonvasin@me.com>
2020-12-10 18:48:54 +0300

    9.2

--- 9.ts
+++ 9.ts
@@ -3,25 +3,55 @@ import { promises as fs } from 'fs';
 let preamble = Number(process.argv[3] || 25);
 let input = (await fs.readFile(process.argv[2], 'utf8')).split('\n').map(Number);
 
-let found: number;
-outer: for (let n = preamble; n < input.length; n++) {
-  let num = input[n];
+let found = input.find((num, n) => {
+  if (n < preamble) {
+    return false;
+  }
 
   for (let i = n - preamble; i < n - 1; i++) {
+    let l = input[i];
+
     for (let j = i + 1; j < n; j++) {
-      let l = input[i];
       let r = input[j];
 
       // found valid pair, moving to next number
       if (l !== r && l + r === num) {
-        continue outer;
+        return false;
         // checked every other pair for this number
       } else if (j === n - 1 && i === n - 2) {
-        found = num;
-        break outer;
+        return true;
       }
+    }
+  }
+});
+
+let highLowSum = 0;
+
+outer: for (let [i, num] of input.entries()) {
+  let sum = num;
+  let low = num;
+  let high = num;
+
+  for (let n = 1; n < input.length; n++) {
+    let cur = input[i + n];
+    sum += cur;
+    if (high < cur) {
+      high = cur;
+    }
+
+    if (low > cur) {
+      low = cur;
+    }
+
+    if (sum === found) {
+      highLowSum = high + low;
+      break outer;
+    }
+
+    if (sum > found) {
+      break;
     }
   }
 }
 
-console.log({ found });
+console.log({ found, highLowSum });
