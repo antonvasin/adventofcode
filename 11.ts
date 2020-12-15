562f13f9f440daf396edc1d8b276e6351c76bc25
Anton Vasin <antonvasin@me.com>
2020-12-15 18:27:57 +0300

    11.2

--- 11.ts
+++ 11.ts
@@ -3,14 +3,18 @@ import { promises as fs } from 'fs';
 let args = process.argv.slice(2);
 let input = (await fs.readFile(args[0], 'utf8')).split('\n');
 let part2 = args[1] === 'true';
-
-type World = string[][];
-
 let maxNeighbours = part2 ? 5 : 4;
 
-function neighbours(world: Map<string, boolean>, k: string) {
-  let count = 0;
+type World = Map<string, boolean>;
+let nghbrs = new Map<string, string[]>();
+
+function neighbours(world: World, k: string) {
+  if (nghbrs.has(k)) {
+    return nghbrs.get(k) as string[];
+  }
+
   let [r, c] = k.split(',').map(Number);
+  let ret: string[] = [];
 
   for (let rOffset = -1; rOffset <= 1; rOffset++) {
     for (let cOffset = -1; cOffset <= 1; cOffset++) {
@@ -22,28 +26,39 @@ function neighbours(world: Map<string, boolean>, k: string) {
         let curR = r + rOffset;
         let curC = c + cOffset;
 
-        if (curR < 0 || curC < 0 || curR >= world.size || curC >= 100) {
-          continue;
-        }
-
-        if (world.get(`${curR},${curC}`) === true) {
-          count++;
-        }
+        let pos = `${curR},${curC}`;
+        ret.push(pos);
       } else {
-        for (let i = 0; ; i++) {
+        for (let i = 1; ; i++) {
           let curR = r + rOffset * i;
           let curC = c + cOffset * i;
+          let pos = `${curR},${curC}`;
 
-          if (curR < 0 || curC < 0 || curR >= world.size || curC >= 100) {
+          if (curR < 0 || curC < 0 || curR > 100 || curC > 100) {
             break;
           }
 
-          if (world.get(`${curR}${curC}`) === true) {
-            count++;
+          if (world.has(pos) === true) {
+            ret.push(pos);
             break;
           }
         }
       }
+    }
+  }
+
+  nghbrs.set(k, ret);
+  return ret;
+}
+
+function countNeighbours(world: World, k: string) {
+  let count = 0;
+
+  let adjacent = neighbours(world, k);
+
+  for (let n of adjacent) {
+    if (world.get(n) === true) {
+      count++;
     }
   }
 
@@ -73,12 +88,12 @@ for (;;) {
 
   for (let [k, occ] of prev) {
     if (occ) {
-      if (neighbours(prev, k) >= maxNeighbours) {
+      if (countNeighbours(prev, k) >= maxNeighbours) {
         occupied.set(k, false);
         changed = true;
       }
     } else {
-      if (neighbours(prev, k) === 0) {
+      if (countNeighbours(prev, k) === 0) {
         occupied.set(k, true);
         changed = true;
       }
