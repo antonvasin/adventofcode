7ebb1079d38867d3bb3ecda298dbc29c781b12a1
Anton Vasin <antonvasin@me.com>
2021-12-10 22:09:24 +0300

    2021 1.2

--- 2021/1.ts
+++ 2021/1.ts
@@ -2,16 +2,44 @@ const testInput = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
 const file = (await Deno.readTextFile('./1-input.txt')).split('\n').map(Number);
 
-let prev = undefined;
-let countInc = 1;
+function partOne(input: number[] = testInput) {
+  let prev = undefined;
+  let countInc = 0;
 
-for (const m of file) {
-  if (prev) {
-    if (m > prev) {
-      countInc++;
+  for (const m of input) {
+    if (prev) {
+      if (m > prev) {
+        countInc++;
+      }
     }
+    prev = m;
   }
-  prev = m;
+
+  console.log('Inc', countInc);
 }
 
-console.log('Inc', countInc);
+function partTwo(input: number[] = testInput) {
+  const threeMeasure = [];
+
+  for (let i = 0; i < input.length; i++) {
+    const element = input[i];
+    const next = input[i + 1];
+    const nextNext = input[i + 2];
+    if (next && nextNext) {
+      threeMeasure.push(element + next + nextNext);
+    }
+  }
+
+  partOne(threeMeasure);
+}
+
+switch (Number(Deno.args[0])) {
+  case 2:
+    partTwo(file);
+    break;
+
+  case 1:
+  default:
+    partOne(file);
+    break;
+}
