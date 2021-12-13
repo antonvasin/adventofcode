662c6955476ddc5595a10c17597912a97c51b62e
Anton Vasin <antonvasin@me.com>
2021-12-13 23:19:45 +0300

    2021 6.2

--- 2021/6.ts
+++ 2021/6.ts
@@ -9,28 +9,34 @@ function parse(input: string) {
 }
 
 function run(input: Input, day = 1) {
-  console.log({ day });
+  let fish: Record<number, number> = {};
 
-  const result = [...input];
+  for (const n of input) {
+    fish[n] ||= 0;
+    fish[n]++;
+  }
 
-  for (let d = 0; d < day; d++) {
-    const lengthBefore = result.length;
+  for (let d = 1; d <= day; d++) {
+    const old = { ...fish };
+    fish = {};
 
-    for (let i = 0; i < result.length; i++) {
-      if (i >= lengthBefore) {
-        break;
+    for (const [k, v] of Object.entries(old)) {
+      const key = Number(k);
+      if (key === 0) {
+        fish[6] ||= 0;
+        fish[6] += v;
+        fish[8] ||= 0;
+        fish[8] += v;
+
+        continue;
       }
 
-      if (result[i] === 0) {
-        result[i] = 6;
-        result.push(8);
-      } else {
-        result[i] -= 1;
-      }
+      fish[key - 1] ||= 0;
+      fish[key - 1] += v;
     }
   }
 
-  return result.length;
+  return Object.entries(fish).reduce((acc, [_, v]) => (acc += v), 0);
 }
 
 runPuzzle(run, parse, './6-input.txt', testInput);
