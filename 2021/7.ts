c57259ca86644295954cfcd5cedc8bd8f2871959
Anton Vasin <antonvasin@me.com>
2021-12-16 01:20:53 +0300

    2021 7.2

--- 2021/7.ts
+++ 2021/7.ts
@@ -7,7 +7,6 @@ const parse = (s: string) => s.split(",").map(Number);
 
 function run(input: Input) {
   const sum = createMap(0);
-  let cheapest;
 
   for (const position of range(Math.min(...input), Math.max(...input) - 1)) {
     for (const crab of input) {
@@ -16,7 +15,32 @@ function run(input: Input) {
     }
   }
 
-  return Object.entries(sum).sort(([_, a], [__, b]) => a - b)[0] as any;
+  const cheapest = Object.entries(sum).sort(([_, a], [__, b]) => a - b)[0];
+
+  return `position ${cheapest[0]}, fuel ${cheapest[1]}`;
 }
 
-runPuzzle(run, parse, "./7-input.txt", testInput);
+function fib(n: number): number {
+  return n === 0 ? 0 : n === 1 ? 1 : n + fib(n - 1);
+}
+
+console.log(fib(3));
+
+function run2(input: Input) {
+  const sum = createMap(0);
+
+  for (const position of range(Math.min(...input), Math.max(...input) - 1)) {
+    for (const crab of input) {
+      const diff = Math.abs(position - crab);
+      const fuel = fib(diff);
+
+      sum[position] += fuel;
+    }
+  }
+
+  const cheapest = Object.entries(sum).sort(([_, a], [__, b]) => a - b)[0];
+
+  return `position ${cheapest[0]}, fuel ${cheapest[1]}`;
+}
+
+runPuzzle([run, run2], parse, "./7-input.txt", testInput);
